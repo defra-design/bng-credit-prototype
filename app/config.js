1a7d5d1ede94d2e6676be2d21839b4d69d93391e
Karl Parton <karlparton@gmail.com>
2023-05-10 14:24:35 +0100

    Updated service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Record biodiversity gains',
+  serviceName: 'Manage biodiversity gain',
 
   // Default port that prototype runs on
   port: '4000',
